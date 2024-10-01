bece81808c53bb3fb125be3cf71cb67e741788ed
HeberRios <68972507+HeberRios@users.noreply.github.com>
2024-10-01 13:22:20 -0600

    Validate the entire date at button press
    When the calculate button is pressed, the 3 inputs will be validated, if
    nothing is wrong, then individually the entire date will be validated,
    if there is something wrong with the date the UI will be updated with
    the respective message.

--- main.js
+++ main.js
@@ -9,9 +9,9 @@ const calculateBtn = document.querySelector("#calculate");
 
 // REGULAR EXPRESSIONS
 
-const dayRegex = /^(0?[1-9]|[12]\d|3[01])$/;
+const dayRegex = /^(0+[1-9]|[12]\d|3[01])$/;
 const monthRegex = /^(0+[1-9]|1[0-2])$/;
-const yearRegex = /^[1-9]{2}\d{2}$/;
+const yearRegex = /^[1-9]{1}\d{3}$/;
 
 // FUNCTIONS
 
@@ -21,16 +21,19 @@ function validateDay() {
         dayInput.classList.add("error-active");
         dayInput.nextElementSibling.textContent = "This field is required";
         dayInput.nextElementSibling.classList.add("error-active");
+        return false;
     } else if (dayRegex.test(dayInput.value)) {
         dayInput.previousElementSibling.classList.remove("error-active");
         dayInput.classList.remove("error-active");
         dayInput.nextElementSibling.textContent = "Valid day";
         dayInput.nextElementSibling.classList.remove("error-active");
+        return true;
     } else {
         dayInput.previousElementSibling.classList.add("error-active");
         dayInput.classList.add("error-active");
         dayInput.nextElementSibling.textContent = "Must be a valid day";
         dayInput.nextElementSibling.classList.add("error-active");
+        return false;
     }
 }
 
@@ -40,16 +43,19 @@ function validateMonth() {
         monthInput.classList.add("error-active");
         monthInput.nextElementSibling.textContent = "This field is required";
         monthInput.nextElementSibling.classList.add("error-active");
+        return false;
     } else if (monthRegex.test(monthInput.value)) {
         monthInput.previousElementSibling.classList.remove("error-active");
         monthInput.classList.remove("error-active");
         monthInput.nextElementSibling.textContent = "Valid month";
         monthInput.nextElementSibling.classList.remove("error-active");
+        return true;
     } else {
         monthInput.previousElementSibling.classList.add("error-active");
         monthInput.classList.add("error-active");
         monthInput.nextElementSibling.textContent = "Must be a valid month";
         monthInput.nextElementSibling.classList.add("error-active");
+        return false;
     }
 }
 
@@ -61,21 +67,60 @@ function validateYear() {
         yearInput.classList.add("error-active");
         yearInput.nextElementSibling.textContent = "This field is required";
         yearInput.nextElementSibling.classList.add("error-active");
+        return false;
     } else if (+yearInput.value > currentYear) {
         yearInput.previousElementSibling.classList.add("error-active");
         yearInput.classList.add("error-active");
         yearInput.nextElementSibling.textContent = "Must be in the past";
         yearInput.nextElementSibling.classList.add("error-active");
+        return false;
     } else if (yearRegex.test(yearInput.value)) {
         yearInput.previousElementSibling.classList.remove("error-active");
         yearInput.classList.remove("error-active");
         yearInput.nextElementSibling.textContent = "Valid year";
         yearInput.nextElementSibling.classList.remove("error-active");
+        return true;
     } else {
         yearInput.previousElementSibling.classList.add("error-active");
         yearInput.classList.add("error-active");
         yearInput.nextElementSibling.textContent = "Must be a valid year";
         yearInput.nextElementSibling.classList.add("error-active");
+        return false;
+    }
+}
+
+function validateDate() {
+    const monthDays = new Date(
+        +yearInput.value,
+        +monthInput.value,
+        0
+    ).getDate();
+
+    // validateYear();
+    // validateMonth();
+    // validateDay();
+
+    if (validateYear() && validateMonth() && validateDay()) {
+        if (monthDays < +dayInput.value) {
+            dayInput.previousElementSibling.classList.add("error-active");
+            dayInput.classList.add("error-active");
+            dayInput.nextElementSibling.textContent = "Must be a valid date";
+            dayInput.nextElementSibling.classList.add("error-active");
+
+            monthInput.previousElementSibling.classList.add("error-active");
+            monthInput.classList.add("error-active");
+            monthInput.nextElementSibling.textContent = "";
+            monthInput.nextElementSibling.classList.remove("error-active");
+
+            yearInput.previousElementSibling.classList.add("error-active");
+            yearInput.classList.add("error-active");
+            yearInput.nextElementSibling.textContent = "";
+            yearInput.nextElementSibling.classList.remove("error-active");
+        } else {
+            return;
+        }
+    } else {
+        return;
     }
 }
 
@@ -84,3 +129,9 @@ function validateYear() {
 dayInput.addEventListener("input", validateDay);
 monthInput.addEventListener("input", validateMonth);
 yearInput.addEventListener("input", validateYear);
+
+calculateBtn.addEventListener("click", function (e) {
+    e.preventDefault();
+
+    validateDate();
+});
